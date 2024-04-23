77e0a22d05df429b8eb6f261c50c342f69ba00db
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-23 22:08:57 +0300

    Refactoring : Refactor and use DRY principle . For each opperation create a function to activate and deactivate overlay,close and open modal buttons.

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+// Creating functions for close and open modal
 
-//   Create eventListener for close-modal button
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//   Create eventListener for close and overlay  buttons
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
