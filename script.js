46abde821ace3e42a872eae8463982b03265d74b
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-23 21:22:28 +0300

    Create : Create variables to store classes form index.html and create a for loop to loop over classes and buton . Create a add event listener click to show-modal button and create classes for modal and overlay to remove the hidden property form style.css.

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button Clicked');
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
